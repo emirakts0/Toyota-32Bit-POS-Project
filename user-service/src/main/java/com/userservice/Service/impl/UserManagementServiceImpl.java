4cb9d21dd05f40a2056f7d1e47f29d95a2bcd9e8
emirakts0 <115027919+emirakts0@users.noreply.github.com>
2024-05-26 01:28:55 +0300

    Logging was added for the user management service, and edits were made for readability and code flow.

--- user-service/src/main/java/com/userservice/Service/impl/UserManagementServiceImpl.java
+++ user-service/src/main/java/com/userservice/Service/impl/UserManagementServiceImpl.java
@@ -11,18 +11,18 @@ import com.userservice.repository.EmployeeRepository;
 import com.userservice.repository.RoleRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
-
-@RequiredArgsConstructor
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserManagementServiceImpl implements UserManagementService {
 
     private final RoleRepository roleRepository;
@@ -30,10 +30,13 @@ public class UserManagementServiceImpl implements UserManagementService {
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
 
+
     @Override
     public Set<RegisterRequestDto> registerEmployee(Set<RegisterRequestDto> registerRequestDtoSet) {
-        for (RegisterRequestDto request : registerRequestDtoSet) {
+        log.trace("registerEmployee method begins. registerRequestDtoSet size: {}", registerRequestDtoSet.size());
 
+        for (RegisterRequestDto request : registerRequestDtoSet) {
+            log.debug("registerEmployee: Processing registration for username: {}", request.getUsername());
             validateDto(request, null);
 
             request.setPassword(passwordEncoder.encode(request.getPassword()));
@@ -48,8 +51,10 @@ public class UserManagementServiceImpl implements UserManagementService {
             employee.setRoles(existingRoles);
             employee.setRegistrationDate(LocalDateTime.now());
             employeeRepository.save(employee);
+            log.info("registerEmployee: Registered employee, ID : {}, username : {}", employee.getId(), employee.getUsername());
         }
 
+        log.trace("registerEmployee method ends. registerRequestDtoSet size: {}", registerRequestDtoSet.size());
         return registerRequestDtoSet;
     }
 
@@ -57,11 +62,11 @@ public class UserManagementServiceImpl implements UserManagementService {
     @Transactional
     @Override
     public UpdateRequestDto updateEmployeeById(Long id, UpdateRequestDto updateRequestDto) {
+        log.trace("updateEmployeeById method begins. ID: {}", id);
 
         validateDto(updateRequestDto, id);
 
         String password = null;
-
         if(updateRequestDto.getPassword() != null) {
             password = passwordEncoder.encode(updateRequestDto.getPassword());
             updateRequestDto.setPassword(password);
@@ -78,98 +83,149 @@ public class UserManagementServiceImpl implements UserManagementService {
 
 
         Set<RoleDto> dtoRoles = updateRequestDto.getRoles();
-
         if (dtoRoles != null && !dtoRoles.isEmpty()) {
-            Employee existingEmployee = employeeRepository.findById(id)
-                    .orElseThrow(() -> new EmployeeNotFoundException(String.format("Employee with ID %d not found", id)));
+            Employee existingEmployee = findAndCheckEmployeeById(id);
 
             Set<Role> existingRoles = new HashSet<>();
             for (RoleDto roleDto : updateRequestDto.getRoles()) {
                 Optional<Role> existingRoleOptional = roleRepository.findByRoleName(roleDto.getRoleName());
                 existingRoleOptional.ifPresent(existingRoles::add);
+                log.debug("updateEmployeeById: Role {} found and added for user ID: {}", roleDto.getRoleName(), id);
             }
 
             existingEmployee.setRoles(existingRoles);
             employeeRepository.save(existingEmployee);
         }
 
+        log.info("updateEmployeeById: updated employee, ID: {}", id);
+        log.trace("updateEmployeeById method ends. ID: {}", id);
         return updateRequestDto;
     }
-
 
 
     @Transactional
     @Override
     public Long deleteEmployeeById(Long id) {
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException(String.format("Employee with ID %d not found", id)));
+        log.trace("deleteEmployeeById method begins. ID: {}", id);
 
-        if (employee.isDeleted())
+        Employee employee = findAndCheckEmployeeById(id);
+
+        if (employee.isDeleted()) {
+            log.warn("deleteEmployeeById: Attempted to delete employee with ID {} but already deleted", id);
             throw new EmployeeAlreadyDeletedException(String.format("Employee with ID %d already deleted", id));
+        }
 
         employeeRepository.markAsDeleted(id);
+        log.info("deleteEmployeeById: Marked employee as deleted, ID: {}", id);
+
+        log.trace("deleteEmployeeById method ends. ID: {}", id);
         return id;
     }
+
 
     @Transactional
     @Override
     public Long reactivateEmployeeById(Long id) {
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException(String.format("Employee with ID %d not found", id)));
+        log.trace("reactivateEmployeeById method begins. ID: {}", id);
 
-        if (!employee.isDeleted())
+        Employee employee = findAndCheckEmployeeById(id);
+
+        if (!employee.isDeleted()) {
+            log.warn("reactivateEmployeeById: Attempted to reactivate employee with ID {} but are not marked as deleted", id);
             throw new EmployeeAlreadyExistsException(String.format("Employee with ID %d already exists", id));
+        }
 
         employee.setDeleted(false);
         employeeRepository.save(employee);
+        log.info("reactivateEmployeeById: Reactivated employee, ID: {}", id);
 
+        log.trace("reactivateEmployeeById method ends. ID: {}", id);
         return id;
     }
 
 
-//-----------------------------------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------------------------
+    private Employee findAndCheckEmployeeById(Long id) {
+        log.trace("findAndCheckEmployeeById method begins. ID: {}", id);
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.warn("findAndCheckEmployeeById: Employee with ID {} not found", id);
+                    return new EmployeeNotFoundException(String.format("Employee with ID %d not found", id));
+                });
+
+        log.trace("findAndCheckEmployeeById method ends. ID: {}", id);
+        return employee;
+    }
+
+
     private void validateDto(Object requestDto, Long id) {
+        log.trace("validateDto method begins.");
 
         if (requestDto instanceof RegisterRequestDto registerRequest) {
-
-            employeeRepository.findByUsername(registerRequest.getUsername())
-                    .ifPresent(u -> {
-                        throw new TakenUsernameException(String.format("Username: %s is taken", registerRequest.getUsername()));
-                    });
-
-            employeeRepository.findByEmail(registerRequest.getEmail())
-                    .ifPresent(e -> {
-                        throw new TakenEmailException(String.format("Email: %s is taken", registerRequest.getEmail()));
-                    });
-
+            registerReqDtoControl(registerRequest);
         }
         else if (requestDto instanceof UpdateRequestDto updateRequest) {
+            updateReqDtoControl(id, updateRequest);
+        }
 
-            Employee existingEmployee = employeeRepository.findByIdAndDeletedFalse(id)
-                    .orElseThrow(() -> new EmployeeNotFoundException(String.format("Employee with ID %d not found", id)));
+        log.trace("validateDto method ends.");
+    }
 
-            if (updateRequest.getUsername() != null) {
-                if (updateRequest.getUsername().equals(existingEmployee.getUsername())) {
-                    throw new TakenUsernameException(String.format("Username: %s is already your current username", updateRequest.getUsername()));
-                } else {
-                    employeeRepository.findByUsername(updateRequest.getUsername())
-                            .ifPresent(u -> {
-                                throw new TakenUsernameException(String.format("Username: %s is taken", updateRequest.getUsername()));
-                            });
-                }
-            }
-            if (updateRequest.getEmail() != null) {
-                if (updateRequest.getEmail().equals(existingEmployee.getEmail())) {
-                    throw new TakenEmailException(String.format("Email: %s is already your current email", updateRequest.getEmail()));
-                } else {
-                    employeeRepository.findByEmail(updateRequest.getEmail())
-                            .ifPresent(e -> {
-                                throw new TakenEmailException(String.format("Email: %s is taken", updateRequest.getEmail()));
-                            });
-                }
+
+    private void registerReqDtoControl(RegisterRequestDto registerRequest) {
+        log.trace("registerReqDtoControl method begins for username: {}", registerRequest.getUsername());
+
+        employeeRepository.findByUsername(registerRequest.getUsername())
+                .ifPresent(u -> {
+                    log.warn("registerReqDtoControl: Username: {} is already taken", registerRequest.getUsername());
+                    throw new TakenUsernameException(String.format("Username: %s is taken", registerRequest.getUsername())); });
+
+        employeeRepository.findByEmail(registerRequest.getEmail())
+                .ifPresent(e -> {
+                    log.warn("registerReqDtoControl: Email: {} is already taken", registerRequest.getEmail());
+                    throw new TakenEmailException(String.format("Email: %s is taken", registerRequest.getEmail())); });
+
+        log.trace("registerReqDtoControl method ends for username: {}", registerRequest.getUsername());
+    }
+
+
+    private void updateReqDtoControl(Long id, UpdateRequestDto updateRequest) {
+        log.trace("updateReqDtoControl method begins for employee ID: {}", id);
+
+        Employee existingEmployee = employeeRepository.findByIdAndDeletedFalse(id)
+                .orElseThrow(() -> {
+                    log.warn("updateReqDtoControl: Employee with ID {} not found", id);
+                    return new EmployeeNotFoundException(String.format("Employee with ID %d not found", id)); });
+
+        if (updateRequest.getUsername() != null) {
+
+            if (updateRequest.getUsername().equals(existingEmployee.getUsername())) {
+                log.warn("updateReqDtoControl: Username: {} is already the current username for employee with ID {}", updateRequest.getUsername(), id);
+                throw new TakenUsernameException(String.format("Username: %s is already your current username", updateRequest.getUsername()));
+
+            } else {
+                employeeRepository.findByUsername(updateRequest.getUsername())
+                        .ifPresent(u -> {
+                            log.warn("updateReqDtoControl: Username: {} is already taken", updateRequest.getUsername());
+                            throw new TakenUsernameException(String.format("Username: %s is taken", updateRequest.getUsername())); });
             }
         }
+
+        if (updateRequest.getEmail() != null) {
+            if (updateRequest.getEmail().equals(existingEmployee.getEmail())) {
+                log.warn("updateReqDtoControl: Email: {} is already the current email for employee with ID {}", updateRequest.getEmail(), id);
+                throw new TakenEmailException(String.format("Email: %s is already your current email", updateRequest.getEmail()));
+            } else {
+                employeeRepository.findByEmail(updateRequest.getEmail())
+                        .ifPresent(e -> {
+                            log.warn("updateReqDtoControl: Email: {} is already taken", updateRequest.getEmail());
+                            throw new TakenEmailException(String.format("Email: %s is taken", updateRequest.getEmail()));
+                        });
+            }
+        }
+
+        log.trace("updateReqDtoControl method ends for employee ID: {}", id);
     }
+
 }
-
-
