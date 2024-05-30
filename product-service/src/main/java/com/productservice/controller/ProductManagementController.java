c8c3d9e0007c6371eb3375b918461b8bc021a7dc
emirakts0 <115027919+emirakts0@users.noreply.github.com>
2024-05-31 02:05:24 +0300

    Logging was added for ProductSearchController and ProductManagementController.

--- product-service/src/main/java/com/productservice/controller/ProductManagementController.java
+++ product-service/src/main/java/com/productservice/controller/ProductManagementController.java
@@ -5,6 +5,7 @@ import com.productservice.dto.UpdateProductRequestDto;
 import com.productservice.service.ProductManagementService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @RestController
 @Validated
+@Slf4j
 @RequestMapping("/product/management")
 public class ProductManagementController {
 
@@ -24,6 +26,7 @@ public class ProductManagementController {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> addProduct(@RequestPart(value = "file", required = false) MultipartFile file,
                                              @Valid @RequestPart(value = "product") ProductCreateRequestDto productCreateRequestDto){
+        log.trace("addProduct endpoint called with product data: {}", productCreateRequestDto);
 
         productManagementService.addProduct(productCreateRequestDto, file);
         return ResponseEntity.status(HttpStatus.CREATED).body("Product saved successfully");
@@ -34,6 +37,7 @@ public class ProductManagementController {
     public ResponseEntity<String> updateProduct(@RequestPart(value = "file", required = false) MultipartFile file,
                                                 @Valid @RequestPart(value = "product", required = false) UpdateProductRequestDto updateProductRequestDto,
                                                 @PathVariable String barcode){
+        log.trace("updateProduct endpoint called for barcode: {}", barcode);
 
         productManagementService.updateProduct(barcode, updateProductRequestDto,file);
         return ResponseEntity.ok().body( "Product is updated successfully" );
@@ -43,6 +47,7 @@ public class ProductManagementController {
     @PutMapping("/{barcode}/stock")
     public ResponseEntity<String> updateStock(@PathVariable String barcode,
                                               @RequestParam int stockChange) {
+        log.trace("updateStock endpoint called for barcode: {} with stock change: {}", barcode, stockChange);
 
         String response = productManagementService.updateStock(barcode, stockChange);
         return ResponseEntity.ok(response);
@@ -51,6 +56,7 @@ public class ProductManagementController {
 
     @DeleteMapping("/{barcode}")
     public ResponseEntity<String> deleteProduct(@PathVariable String barcode){
+        log.trace("deleteProduct endpoint called for barcode: {}", barcode);
 
         productManagementService.deleteProductByBarcode(barcode);
         return ResponseEntity.ok().body(String.format("The product with the barcode %s has been deleted.", barcode));
@@ -59,6 +65,7 @@ public class ProductManagementController {
 
     @PutMapping("/re-add/{barcode}")
     public ResponseEntity<String> reAddDeletedProduct(@PathVariable String barcode) {
+        log.trace("reAddDeletedProduct endpoint called for barcode: {}", barcode);
 
         productManagementService.reAddDeletedProductByBarcode(barcode);
         return ResponseEntity.ok().body(String.format("The product with the barcode %s has been re-added.", barcode));
@@ -67,6 +74,7 @@ public class ProductManagementController {
 
     @DeleteMapping("/delete-image/{barcode}")
     public ResponseEntity<String> deleteProductImage(@PathVariable String barcode){
+        log.trace("deleteProductImage endpoint called for barcode: {}", barcode);
 
         productManagementService.deleteImageByBarcode(barcode);
         return ResponseEntity.ok().body(String.format("The product image with the barcode %s has been deleted.", barcode));
